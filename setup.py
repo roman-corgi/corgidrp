1dce83f77307de9ba400f7b3ad98da1fe0895b5c
Jason Wang <jason.wang@outlook.com>
2024-01-10 15:25:26 -0600

    breaking: caldb first implementation

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
 from setuptools import setup, find_packages
+import os
+import pathlib
+import configparser
+
+## Create a configuration file for the corgidrp if it doesn't exist. 
+homedir = pathlib.Path.home()
+config_filepath = os.path.join(homedir, ".corgidrp")
+if not os.path.exists(config_filepath):
+    corgidrp_basedir = os.path.dirname(__file__)
+    config = configparser.ConfigParser()
+    config["PATH"] = {}
+    config["PATH"]["caldb"] = os.path.join(corgidrp_basedir, "corgidrp_caldb.csv") # location to store caldb
+    config["PATH"]["auxdata"] = os.path.join(corgidrp_basedir, "aux") + os.path.sep # folder for auxiliary data
+    with open(config_filepath, 'w') as f:
+            config.write(f)
+
+    print("corgidrp: Configuration file written to {0}. Please edit if you want things stored in different locations.".format(config_filepath))
 
 def get_requires():
     reqs = []
