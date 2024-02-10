7dd31bf76c153fb5bd2aa7ecc92f8113bb6f464b
Maxwell Millar-Blanchaer <maxmb.astro@gmail.com>
2024-02-10 12:21:37 -0800

    moved dark_subtraction from detector.py to l2a_to_l2b.py

--- corgidrp/detector.py
+++ corgidrp/detector.py
@@ -1,3 +1,5 @@
+# Place to put detector-related utility functions
+
 import numpy as np
 import corgidrp.data as data
 
@@ -18,27 +20,3 @@ def create_dark_calib(dark_dataset):
                          ext_hdr=dark_dataset[0].ext_hdr.copy(), input_dataset=dark_dataset)
 
     return new_dark
-
-
-def dark_subtraction(input_dataset, dark_frame):
-    """
-    Perform dark current subtraction of a dataset using the corresponding dark frame
-
-    Args:
-        input_dataset (corgidrp.data.Dataset): a dataset of Images that need dark subtraction (L2a-level)
-        dark_frame (corgidrp.data.Dark): a Dark frame to model the dark current
-
-    Returns:
-        corgidrp.data.Dataset: a dark subtracted version of the input dataset
-    """
-    # you should make a copy the dataset to start
-    darksub_dataset = input_dataset.copy()
-
-    darksub_cube = darksub_dataset.all_data - dark_frame.data
-
-    history_msg = "Dark current subtracted using dark {0}".format(dark_frame.filename)
-
-    # update the output dataset with this new dark subtracted data and update the history
-    darksub_dataset.update_after_processing_step(history_msg, new_all_data=darksub_cube)
-
-    return darksub_dataset
