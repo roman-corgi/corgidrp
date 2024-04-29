5c099004cefcb4ec381dd8c5d5775b3b70a5dd46
Lisa <lisa.altinier@lam.fr>
2024-04-29 11:26:34 +0200

    Function that divides by the exposure time
    The function divides data and error by the exposure time. Header updated. Test to check that the function works.

--- corgidrp/l2b_to_l3.py
+++ corgidrp/l2b_to_l3.py
@@ -1,4 +1,5 @@
 # A file that holds the functions that transmogrify l2b data to l3 data 
+import numpy as np
 
 def create_wcs(input_dataset):
     """
@@ -19,8 +20,6 @@ def divide_by_exptime(input_dataset):
     
     Divide the data by the exposure time to get the units in electrons/s
 
-    TODO: Make sure to update the headers to reflect the change in units
-
     Args:
         input_dataset (corgidrp.data.Dataset): a dataset of Images (L2b-level)
 
@@ -28,4 +27,25 @@ def divide_by_exptime(input_dataset):
         corgidrp.data.Dataset: a version of the input dataset with the data in units of electrons/s
     """
 
-    return None
+    data = input_dataset.copy()
+
+    all_data_new = np.zeros(data.all_data.shape)
+    all_err_new = np.zeros(data.all_err.shape)
+
+    for i in range(len(data.frames)):
+        exposure_time = float(data.frames[i].ext_hdr['EXPTIME']) #ask if it is the same 
+
+        data.frames[i].data = data.frames[i].data / exposure_time
+        data.frames[i].err = data.frames[i].err / exposure_time
+
+        all_data_new[i] = data.frames[i].data
+        all_err_new[i] = data.frames[i].err
+
+        data.frames[i].ext_hdr.set('EXPTIME', 1 )
+        data.frames[i].ext_hdr.set('UNITS', 'electrons/s' )
+
+
+    history_msg = "divided by the exposure time"
+    data.update_after_processing_step(history_msg, new_all_data=all_data_new, new_all_err = all_err_new)
+
+    return data
