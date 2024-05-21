7854b2932f5971f57ded9cfb1d05882d8c3b853f
Sergi Hildebrandt <Sergi.Hildebrandt.Rafels@jpl.nasa.gov>
2024-05-21 15:54:48 -0700

    Update test_rowreadtime.py

--- tests/test_rowreadtime.py
+++ tests/test_rowreadtime.py
@@ -5,12 +5,17 @@ from corgidrp.detector import get_rowreadtime_sec
 rowreadtime_sec = 0.0002235
 
 def test_rowreadtime():
-    # Valid datetime
+    # Default datetime
+    rowreadtime_sec_test = get_rowreadtime_sec()
+    
+    assert rowreadtime_sec == rowreadtime_sec_test
+
+    # Another valid datetime
     rowreadtime_sec_test = get_rowreadtime_sec(datetime=Time('2024-05-16 00:00:00',
         scale='utc'))
 
     assert rowreadtime_sec == rowreadtime_sec_test
-    
+
 if __name__ == '__main__':
     test_rowreadtime()
 
