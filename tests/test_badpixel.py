6bfa02318979aa536bfee42f039ec1cf11ce256d
Sergi Hildebrandt <Sergi.Hildebrandt.Rafels@jpl.nasa.gov>
2024-06-01 11:56:56 -0700

    UT finished and tested

--- tests/test_badpixel.py
+++ tests/test_badpixel.py
@@ -18,7 +18,7 @@ def test_bad_pixels():
 
     corgidrp.track_individual_errors = True
 
-    print("test correct bad pixels pipeline step")
+    print("UT for pipeline step correct_bad_pixels")
 
     image1 = Image(data, pri_hdr = prhd, ext_hdr = exthd, err = err, dq = dq)
     dataset = Dataset([image1])
@@ -28,10 +28,10 @@ def test_bad_pixels():
     data_cube = dataset.all_data # 3D data cube for all frames in the dataset
     dq_cube = dataset.all_dq # 3D DQ array cube for all frames in the dataset
     # Add some CR
-    col_cr = [12, 123, 234, 456, 678, 890]
-    row_cr = [546, 789, 123, 43, 547, 675]
-    for i_col in col_cr:
-        for i_row in row_cr:
+    col_cr_test = [12, 123, 234, 456, 678, 890]
+    row_cr_test = [546, 789, 123, 43, 547, 675]
+    for i_col in col_cr_test:
+        for i_row in row_cr_test:
             dq_cube[0, i_col, i_row] += 128
 
     history_msg = "Pixels affected by CR added"
@@ -44,48 +44,48 @@ def test_bad_pixels():
     datadir = os.path.join(os.path.dirname(__file__), "simdata")
     if not os.path.exists(datadir):
         os.mkdir(datadir)
+    col_bp_test=[12, 120, 234, 450, 678, 990]
+    row_bp_test=[546, 89, 123, 243, 447, 675]
     bp_mask = mocks.create_badpixelmap_files(filedir=datadir,
-        col_bp=[12, 120, 234, 450, 678, 990],
-        row_bp=[546, 89, 123, 243, 447, 675])
-    breakpoint()
-    new_bp_mask = data.BadPixelMap(bp_mask.all_data, pri_hdr=bp_mask[0].pri_hdr.copy(),
-                         ext_hdr=bp_mask[0].ext_hdr.copy(), input_dataset=bp_mask)
+        col_bp=col_bp_test, row_bp=row_bp_test)
+    new_bp_mask = BadPixelMap(bp_mask.all_data, pri_hdr=bp_mask[0].pri_hdr.copy(),
+                     ext_hdr=bp_mask[0].ext_hdr.copy(), input_dataset=bp_mask)
 
-    assert type(dataset) == corgidrp.data.BadPixelMap
+    assert type(new_bp_mask) == corgidrp.data.BadPixelMap
 
     new_dataset = correct_bad_pixels(dataset, bp_mask)
 
     assert type(new_dataset) == corgidrp.data.Dataset
 
     # Checking that NaN are only at the expected locations
-    nan_data = np.where(new_dataset.all_data[0] == np.nan)
-    for ii in nan_data[0]:
-        assert ii in col_cr or ii in col_bp
-    for jj in nan_data[1]:
-        assert jj in row_cr or jj in row_bp
-
-    # Checking that CR are at the expected locations
-    cr_dq = np.where(new_dataset.all_dq[0] == 4)
-    for ii in cr_dq[0]:
-        assert ii in col_cr
-    for jj in cr_dq[1]:
-        assert jj in row_cr
+    nan_data = np.argwhere(np.isnan(new_dataset.all_data[0]))
+    for ii in nan_data[:,0]:
+        assert ii in col_cr_test or ii in col_bp_test
+    for jj in nan_data[:,1]:
+        assert jj in row_cr_test or jj in row_bp_test
 
     # Checking that bad pixels are at the expected locations
-    bp_dq = np.where(new_dataset.all_dq[0] == 128)
+    bp_dq = np.where(new_dataset.all_dq[0] == 4)
     for ii in bp_dq[0]:
-        assert ii in col_bp
+        assert ii in col_bp_test
     for jj in bp_dq[1]:
-        assert jj in row_bp
+        assert jj in row_bp_test
+
+    # Checking that CR are at the expected locations
+    cr_dq = np.where(new_dataset.all_dq[0] == 128)
+    for ii in cr_dq[0]:
+        assert ii in col_cr_test
+    for jj in cr_dq[1]:
+        assert jj in row_cr_test
 
     # Checking that coincident bad pixels and CR are at the expected locations
     bp_cr_dq = np.where(new_dataset.all_dq[0] == 132)
     for ii in bp_cr_dq[0]:
-        assert ii in col_bp and ii in col_cr
+        assert ii in col_bp_test and ii in col_cr_test
     for jj in bp_cr_dq[1]:
-        assert jj in row_bp and jj in row_cr
+        assert jj in row_bp_test and jj in row_cr_test
 
-    breakpoint()
+    print("UT passed")
 
 if __name__ == '__main__':
     test_bad_pixels()
