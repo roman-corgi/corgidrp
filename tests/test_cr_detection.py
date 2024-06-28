51be4c9c1b3a3960c6098552e8bbbf79bdb36377
Kevin Ludwick <kevin.ludwick.ext@ierustech.com>
2024-06-27 22:37:21 -0500

    lint

--- tests/test_cr_detection.py
+++ tests/test_cr_detection.py
@@ -77,29 +77,29 @@ def remove_cosmics_iit(image, fwc, sat_thresh, plat_thresh, cosm_filter, cosm_bo
     be identified. A reasonable value is 2.
 
     Args:
-        image : array_like, float
+        image (array_like, float):
         Image area of frame (bias of zero).
-        fwc : float
+        fwc (float):
         Full well capacity of detector *in DNs*.  Note that this may require a
         conversion as FWCs are usually specified in electrons, but the image
         is in DNs at this point.
-        sat_thresh : float
+        sat_thresh (float):
         Multiplication factor for fwc that determines saturated cosmic pixels.
-        plat_thresh : float
+        plat_thresh (float):
         Multiplication factor for fwc that determines edges of cosmic plateau.
-        cosm_filter : int
+        cosm_filter (int):
         Minimum length in pixels of cosmic plateaus to be identified.
-        cosm_box : int
+        cosm_box (int):
         Number of pixels out from an identified cosmic head (i.e., beginning of
         the plateau) to mask out.
         For example, if cosm_box is 3, a 7x7 box is masked,
         with the cosmic head as the center pixel of the box.
-        cosm_tail : int
+        cosm_tail (int):
         Number of pixels in the row downstream of the end of a cosmic plateau
         to mask.  If cosm_tail is greater than the number of
         columns left to the end of the row from the cosmic
         plateau, the cosmic masking ends at the end of the row.
-        mode : string
+        mode (string):
         If 'image', an image-area input is assumed, and if the input
         tail length is longer than the length to the end of the image-area row,
         the mask is truncated at the end of the row.
@@ -108,7 +108,7 @@ def remove_cosmics_iit(image, fwc, sat_thresh, plat_thresh, cosm_filter, cosm_bo
         continues onto the next row.  Defaults to 'image'.
 
     Returns:
-        mask : array_like, int
+        mask (array_like, int):
         Mask for pixels that have been set to zero.
 
     Notes
