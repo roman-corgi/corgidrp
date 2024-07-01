96151e50d47ecbb6fb42f746837190e89d9d5a5e
manduhmia <79128537+manduhmia@users.noreply.github.com>
2024-07-01 12:52:33 -0500

    adding more assertions

--- tests/test_astrom.py
+++ tests/test_astrom.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import corgidrp
 from corgidrp import data, mocks, astrom
+import astropy.io.ascii as ascii
 
 def test_astrom():
     """ 
@@ -38,6 +39,13 @@ def test_astrom():
     # check orientation is correct within 0.3 [deg]
     assert np.abs(expected_platescale - astrom_cal.platescale) < 0.5  # not sure how accurate this needs to be
     assert np.abs(expected_northangle - astrom_cal.northangle) < 0.3
+
+    # check that derived center coordinate falls within the field limits/ skycoords used
+    guesses = ascii.read(guess_path)
+    assert astrom_cal.boresight[0] >= np.min(guesses['RA'])
+    assert astrom_cal.boresight[0] <= np.max(guesses['RA'])
+    assert astrom_cal.boresight[1] >= np.min(guesses['DEC'])
+    assert astrom_cal.boresight[1] <= np.max(guesses['DEC'])
 
 if __name__ == "__main__":
     test_astrom()
