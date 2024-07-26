41f526e9b05f59570311be1e71b057fd765cd694
Sergi Hildebrandt <srh@caltech.edu>
2024-07-25 17:16:38 -0700

    Nonlin: number of frames as a parameter
    Similar to Kgain: The required number of frames for nonlin cal is now a parameter and unit tests use as few as possible to reduce the required RAM used during the test
    
    One can optionally run the original tests with a set of sim data that resembles real IIT data, too, by setting False at the beginning of test_nonlin_cal.py

--- tests/test_nonlin_cal.py
+++ tests/test_nonlin_cal.py
@@ -34,6 +34,27 @@ exp_time_stack_arr0 = loaded['array1']
 time_stack_arr0 = loaded['array2']
 len_list0 = loaded['array3']
 gain_arr0 = loaded['array4'] # G = 1, 2, 10, 20
+# Reducing the number of frames used in unit tests (each has 5 substacks)
+n_cal = 1 
+idx_0 = 0
+idx_1 = 0
+# Turn into False and the test runs a similar amount of data as with real data in IIT (~40 Gb RAM)
+rms_test = 0.0035 # Value used when using an equivalent dataset as in IIT
+if True:
+    rms_test = 0.04 # Less strict comparison sue to having significantly less frames
+    for iG in range(len(len_list0)):
+        time_stack_arr0[idx_0:idx_0+n_cal*5] = time_stack_arr0[idx_1:idx_1+n_cal*5]
+        for i_cal in range(n_cal):
+            # There must be at least two frames with the same exposure time
+            exp_time_stack_arr0[idx_0:idx_0+2] = exp_time_stack_arr0[idx_1]
+            exp_time_stack_arr0[idx_0+2:idx_0+5] = exp_time_stack_arr0[idx_1]+ 0.05
+            idx_0 += 5
+        idx_1 += len_list0[iG]
+    # Remove unused data
+    exp_time_stack_arr0 = np.delete(exp_time_stack_arr0, idx_0 + np.arange(idx_1-idx_0))
+    time_stack_arr0 = np.delete(time_stack_arr0, idx_0 + np.arange(idx_1-idx_0))
+    # Update len_list0
+    len_list0[:] = n_cal*5
 
 # Load the flux map
 fluxmap_init = np.load(Path(here,'test_data','FluxMap1024.npy'))
@@ -45,7 +66,8 @@ fluxMap4 = 0.04*fluxmap_init # e/s/px, for G = 20
 
 # detector parameters
 kgain = 8.7 # e-/DN
-rn = 130 # read noise in e-
+# Usual number of frames to deal with real rn values is ~200
+rn = 130/np.sqrt(200/n_cal) # read noise in e-
 bias = 2000 # e-
 
 # cubic function nonlinearity for emgain of 1
@@ -58,8 +80,9 @@ else:
 # commanded EM gain
 emgain = 1.0
 frame_list = []
-# make 30 uniform frames with emgain = 1
-for j in range(30):
+# make some uniform frames with emgain = 1
+n_mean = 3
+for j in range(n_mean):
     image_sim = make_fluxmap_image(fluxMap1,bias,kgain,rn,emgain,5.0,coeffs_1,
         nonlin_flag=nonlin_flag)
     # Datetime cannot be duplicated
@@ -119,7 +142,7 @@ max_write = 10000
 def test_expected_results_nom_sub():
     """Outputs are as expected for the provided frames with nominal arrays."""
     nonlin_out = calibrate_nonlin(dataset_nl, actual_gain_arr,
-        actual_gain_mean_frame, norm_val, min_write, max_write)
+        actual_gain_mean_frame, n_cal, n_mean, norm_val, min_write, max_write)
         
     # Calculate rms of the differences between the assumed nonlinearity and 
     # the nonlinearity determined with calibrate_nonlin
@@ -132,12 +155,13 @@ def test_expected_results_nom_sub():
     rms2 = np.sqrt(np.mean(diffs1**2))
     rms3 = np.sqrt(np.mean(diffs2**2))
     rms4 = np.sqrt(np.mean(diffs3**2))
-        
-    # check that the four rms values are below the max value
-    assert np.less(rms1,0.0035)
-    assert np.less(rms2,0.0035)
-    assert np.less(rms3,0.0035)
-    assert np.less(rms4,0.0035)
+
+    # check that the four rms values are below some value (real data take
+    # several frames and the value in IIT was 0.0035 for all of them)
+    assert np.less(rms1,rms_test)
+    assert np.less(rms2,rms_test)
+    assert np.less(rms3,rms_test)
+    assert np.less(rms4,rms_test)
     # check that the first element in the first column is equal to min_write
     assert np.equal(nonlin_out.data[1,0], min_write)
     # check that the last element in the first column is equal to max_write
@@ -154,8 +178,8 @@ def test_expected_results_time_sub():
     one EM gain group taken 1 day later. Set (gain_arr0[3]) taken 1 day later
     avoiding a duplication of the whole dataset."""
     time_stack_arr1 = np.copy(time_stack_arr0)
-    # Convert date-time strings to datetime objects. First 30 are used to create a mean frame
-    index = len(dataset_nl) - 30 - len_list0[3]
+    # Convert date-time strings to datetime objects. First ones are used to create a mean frame
+    index = len(dataset_nl) - n_mean - len_list0[3]
     ctime_datetime1 = pd.to_datetime(time_stack_arr1[index:], errors='coerce')
     # Add one day to each datetime object
     ctime_datetime_plus_one_day1 = ctime_datetime1 + pd.Timedelta(days=1)
@@ -163,8 +187,8 @@ def test_expected_results_time_sub():
     ctime_strings_plus_one_day1 = ctime_datetime_plus_one_day1.strftime('%Y-%m-%dT%H:%M:%S').tolist()
     time_stack_arr1[index:index+len_list0[3]] = ctime_strings_plus_one_day1
     index = 0
-    # First 30 are used to create a mean frame
-    idx_frame = 30
+    # First ones are used to create a mean frame
+    idx_frame = n_mean
     for iG in range(len(gain_arr0)):
         exp_time_loop = exp_time_stack_arr0[index:index+len_list0[iG]]
         time_stack_test = time_stack_arr1[index:index+len_list0[iG]]
@@ -173,8 +197,8 @@ def test_expected_results_time_sub():
             dataset_nl[idx_frame].ext_hdr['DATETIME'] = time_stack_test[idx_t]
             idx_frame += 1
 
-    nonlin_out = calibrate_nonlin(dataset_nl, 
-                        actual_gain_arr, actual_gain_mean_frame, norm_val, min_write, max_write)
+    nonlin_out = calibrate_nonlin(dataset_nl, actual_gain_arr, actual_gain_mean_frame,
+        n_cal, n_mean, norm_val, min_write, max_write)
      
     # Calculate rms of the differences between the assumed nonlinearity and 
     # the nonlinearity determined with calibrate_nonlin
@@ -188,18 +212,19 @@ def test_expected_results_time_sub():
     rms3 = np.sqrt(np.mean(diffs2**2))
     rms4 = np.sqrt(np.mean(diffs3**2))
     
-    # check that the four rms values are below the max value
-    assert np.less(rms1,0.0035)
-    assert np.less(rms2,0.0035)
-    assert np.less(rms3,0.0035)
-    assert np.less(rms4,0.0035)
+    # check that the four rms values are below some value (real data take
+    # several frames and the value in IIT was 0.0035 for all of them)
+    assert np.less(rms1,rms_test)
+    assert np.less(rms2,rms_test)
+    assert np.less(rms3,rms_test)
+    assert np.less(rms4,rms_test)
   
 def test_norm_val():
     """norm_val must be divisible by 20."""
     norm_not_div_20 = 2010
     with pytest.raises(CalNonlinException):
         calibrate_nonlin(dataset_nl, actual_gain_arr, actual_gain_mean_frame,
-                            norm_not_div_20, min_write, max_write)
+                            n_cal, n_mean, norm_not_div_20, min_write, max_write)
  
 def test_rps():
     """these two below must be real positive scalars."""
@@ -208,19 +233,19 @@ def test_rps():
     for rerr in check_list:
         with pytest.raises(TypeError):
             calibrate_nonlin(dataset_nl, actual_gain_arr, actual_gain_mean_frame,
-                                norm_val, rerr, max_write)
+                                n_cal, n_mean, norm_val, rerr, max_write)
     # max_write
     for rerr in check_list:
         with pytest.raises(TypeError):
             calibrate_nonlin(dataset_nl, actual_gain_arr, actual_gain_mean_frame,
-                                norm_val, min_write, rerr)
+                                n_cal, n_mean, norm_val, min_write, rerr)
    
 def test_max_gt_min():
     """max_write must be greater than min_write."""
     werr = min_write # set the max_write value to the min_write value
     with pytest.raises(CalNonlinException):
         calibrate_nonlin(dataset_nl, actual_gain_arr, actual_gain_mean_frame,
-                            norm_val, min_write, werr)
+                            n_cal, n_mean, norm_val, min_write, werr)
     
 def test_psi():
     """norm_val must be a positive scalar integer."""
@@ -229,7 +254,7 @@ def test_psi():
     for mmerr in check_list:
         with pytest.raises(TypeError):
             calibrate_nonlin(dataset_nl, actual_gain_arr, actual_gain_mean_frame,
-                                mmerr, min_write, max_write)
+                                n_cal, n_mean, mmerr, min_write, max_write)
  
 if __name__ == '__main__':
     print('Running test_expected_results_nom_sub')
