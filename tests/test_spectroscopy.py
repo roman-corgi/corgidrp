ae0078d3390beebf32ae96edb50560dc0eac969b
Michele Woodland <michele.woodland@nasa.gov>
2024-08-27 22:15:47 -0400

    make file to test spectroscopy

---
+++ tests/test_spectroscopy.py
@@ -0,0 +1,23 @@
+import os
+import numpy as np
+import pytest
+import corgidrp
+import corgidrp.mocks as mocks
+import corgidrp.data as data
+import corgidrp.spectroscopy as spectroscopy
+
+
+def test_spectroscopy():
+    print('testing spectroscopy')
+    x = spectroscopy('file')
+
+# load simulated data files
+# psf template - here is g0v star
+
+# "data" - here is a0v star
+
+
+
+
+if __name__ == "__main__":
+    test_spectroscopy()
