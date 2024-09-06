f57182e4a47c7338aa8e675490c9a063af146853
manduhmia <79128537+manduhmia@users.noreply.github.com>
2024-09-06 10:31:21 -0500

    changing tolerance windows for unit testing

--- tests/test_astrom.py
+++ tests/test_astrom.py
@@ -1,6 +1,8 @@
 import os
 import numpy as np
 import pytest
+import sys
+sys.path.insert(0, '/Users/macuser/Roman/corgidrp')
 import corgidrp
 import corgidrp.mocks as mocks
 import corgidrp.astrom as astrom
@@ -36,17 +38,17 @@ def test_astrom():
     expected_platescale = 21.8
     expected_northangle = 45
 
-    # check orientation is correct within 0.3 [deg]
+    # check orientation is correct within 0.05 [deg]
     # and plate scale is correct within 0.5 [mas] (arbitrary)
     assert astrom_cal.platescale == pytest.approx(expected_platescale, abs=0.5)
-    assert astrom_cal.northangle == pytest.approx(expected_northangle, abs=0.3)
+    assert astrom_cal.northangle == pytest.approx(expected_northangle, abs=0.05)
 
     # check that the center is correct within 30 [mas]
     # the simulated image should have no shift from the target
     target = dataset[0].pri_hdr['RA'], dataset[0].pri_hdr['DEC']
     ra, dec = astrom_cal.boresight[0], astrom_cal.boresight[1]
-    assert ra == pytest.approx(target[0], abs=0.0000083)
-    assert dec == pytest.approx(target[1], abs=0.0000083)
+    assert ra == pytest.approx(target[0], abs=8.333e-7)
+    assert dec == pytest.approx(target[1], abs=8.333e-7)
 
 if __name__ == "__main__":
     test_astrom()
