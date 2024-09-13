b17b13eae2a3974883352f3642c5f6f282bab3a2
Juergen Schreiber <schreiber@mpia.de>
2024-09-13 09:45:35 +0200

    merge from main

--- tests/e2e_tests/kgain_e2e.py
+++ tests/e2e_tests/kgain_e2e.py
@@ -21,7 +21,6 @@ tvac_readnoise = 133 #e
 def test_l1_to_kgain(tvacdata_path, e2eoutput_path):
     # figure out paths, assuming everything is located in the same relative location
     l1_datadir = os.path.join(tvacdata_path, "TV-20_EXCAM_noise_characterization", "kgain")
-    print(l1_datadir)
     l1_datadir_nonlin = os.path.join(tvacdata_path, "TV-20_EXCAM_noise_characterization", "nonlin")
     nonlin_path = os.path.join(tvacdata_path, "TV-20_EXCAM_noise_characterization", "results", "nonlin_table_240322.txt")
     kgain_result = os.path.join(tvacdata_path, "TV-20_EXCAM_noise_characterization", "results", "kgain_read_noise.txt")
@@ -35,9 +34,7 @@ def test_l1_to_kgain(tvacdata_path, e2eoutput_path):
     # define the raw science data to process
 
     l1_data_filelist_same_exp = [os.path.join(l1_datadir, "CGI_EXCAM_L1_00000{0}.fits".format(i)) for i in np.arange(51841,51871)]#[51841, 51851]] # just grab some files of it
-    print(l1_data_filelist_same_exp)
     l1_data_filelist_range_exp = [os.path.join(l1_datadir, "CGI_EXCAM_L1_00000{0}.fits".format(i)) for i in np.arange(51731, 51841)]#[51731, 51761]]
-    print(l1_data_filelist_range_exp)
     l1_data_filelist = [os.path.join(l1_datadir, "CGI_EXCAM_L1_00000{0}.fits".format(i)) for i in np.arange(51731, 51871)]
     mock_cal_filelist = [os.path.join(l1_datadir_nonlin, "CGI_EXCAM_L1_00000{0}.fits".format(i)) for i in [51825, 55165]] # grab some real data to mock the calibration 
     
