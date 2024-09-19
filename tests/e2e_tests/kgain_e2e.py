4abec8a7f07785f9c79b49d1e6af647c5cb8c9c4
Juergen Schreiber <schreiber@mpia.de>
2024-09-19 13:24:23 +0200

    adust detector_region parameters to eng code in kgain recipe

--- tests/e2e_tests/kgain_e2e.py
+++ tests/e2e_tests/kgain_e2e.py
@@ -14,15 +14,13 @@ import corgidrp.caldb as caldb
 
 thisfile_dir = os.path.dirname(__file__) # this file's folder
 
-tvac_kgain = 8.7 #e/DN
-tvac_readnoise = 133 #e
+tvac_kgain = 8.8145 #e/DN
+tvac_readnoise = 130.12 #e
 
 @pytest.mark.e2e
 def test_l1_to_kgain(tvacdata_path, e2eoutput_path):
     # figure out paths, assuming everything is located in the same relative location
     l1_datadir = os.path.join(tvacdata_path, "TV-20_EXCAM_noise_characterization", "kgain")
-    l1_datadir_nonlin = os.path.join(tvacdata_path, "TV-20_EXCAM_noise_characterization", "nonlin")
-    nonlin_path = os.path.join(tvacdata_path, "TV-20_EXCAM_noise_characterization", "results", "nonlin_table_240322.txt")
     kgain_result = os.path.join(tvacdata_path, "TV-20_EXCAM_noise_characterization", "results", "kgain_read_noise.txt")
 
     # make output directory if needed
@@ -35,29 +33,7 @@ def test_l1_to_kgain(tvacdata_path, e2eoutput_path):
 
     l1_data_filelist_same_exp = [os.path.join(l1_datadir, "CGI_EXCAM_L1_00000{0}.fits".format(i)) for i in np.arange(51841,51871)]#[51841, 51851]] # just grab some files of it
     l1_data_filelist_range_exp = [os.path.join(l1_datadir, "CGI_EXCAM_L1_00000{0}.fits".format(i)) for i in np.arange(51731, 51841)]#[51731, 51761]]
-    l1_data_filelist = [os.path.join(l1_datadir, "CGI_EXCAM_L1_00000{0}.fits".format(i)) for i in np.arange(51731, 51871)]
-    mock_cal_filelist = [os.path.join(l1_datadir_nonlin, "CGI_EXCAM_L1_00000{0}.fits".format(i)) for i in [51825, 55165]] # grab some real data to mock the calibration 
     
-    ###### Setup necessary calibration files
-    # Create necessary calibration files
-    # we are going to make a new nonlinear calibration file using
-    # a combination of the II&T nonlinearity file and the mock headers from
-    # our unit test version of the NonLinearityCalibration
-    nonlin_dat = np.genfromtxt(nonlin_path, delimiter=",")
-    pri_hdr, ext_hdr = mocks.create_default_headers()
-    ext_hdr["DRPCTIME"] = time.Time.now().isot
-    ext_hdr['DRPVERSN'] =  corgidrp.__version__
-    mock_input_dataset = data.Dataset(mock_cal_filelist)
-    nonlinear_cal = data.NonLinearityCalibration(nonlin_dat,
-                                                 pri_hdr=pri_hdr,
-                                                 ext_hdr=ext_hdr,
-                                                 input_dataset=mock_input_dataset)
-    nonlinear_cal.save(filedir=kgain_outputdir, filename="mock_nonlinearcal.fits" )
-
-    # add calibration file to caldb
-    this_caldb = caldb.CalDB()
-    this_caldb.create_entry(nonlinear_cal)
-
     ####### Run the walker on some test_data
 
     for file in l1_data_filelist_same_exp:
@@ -72,9 +48,6 @@ def test_l1_to_kgain(tvacdata_path, e2eoutput_path):
     kgain_file = os.path.join(kgain_outputdir, "CGI_EXCAM_L1_0000051731_kgain.fits")
     kgain = data.KGain(kgain_file)
     
-    # clean up by removing entry
-    this_caldb.remove_entry(nonlinear_cal)
-    
     ##### Check against TVAC kgain, readnoise
     new_kgain = kgain.value
     new_readnoise = kgain.ext_hdr["RN"]
@@ -86,10 +59,10 @@ def test_l1_to_kgain(tvacdata_path, e2eoutput_path):
     print ("difference to TVAC kgain:", diff_kgain)
     print ("difference to TVAC read noise:", diff_readnoise)
     print ("error of kgain:", kgain.error)
-    print ("error of  readnoise:", kgain.ext_hdr["RN_ERR"])
+    print ("error of readnoise:", kgain.ext_hdr["RN_ERR"])
 
-    assert np.abs(diff_kgain) < 0.1
-    assert np.abs(diff_readnoise) < 1
+    assert np.abs(diff_kgain) < 0.01
+    assert np.abs(diff_readnoise) < 3
 
     
 if __name__ == "__main__":
