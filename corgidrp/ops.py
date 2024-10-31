68ac7e5cc2cebe0deccc3c473b1c8cb4fbf021f1
Maxwell A. Millar-Blanchaer <maxmb@ucsb.edu>
2024-10-31 12:05:22 -0700

    simplified function names

--- corgidrp/ops.py
+++ corgidrp/ops.py
@@ -4,13 +4,13 @@ import corgidrp.walker as walker
 import argparse
 
 
-def step_1_for_all_data_initialize():
+def step_1_initialize():
     this_caldb = caldb.CalDB()
     return this_caldb
 
-def step_2_for_all_data_load_cal(this_caldb, main_cal_dir):
+def step_2_load_cal(this_caldb, main_cal_dir):
     this_caldb.scan_dir_for_new_entries(main_cal_dir)
     return this_caldb
 
-def step_3_for_all_data_process(input_filelist, cpgs_xml_filepath, outputdir, template =None):
+def step_3_process_data(input_filelist, cpgs_xml_filepath, outputdir, template =None):
      walker.walk_corgidrp(input_filelist, cpgs_xml_filepath, outputdir, template=template)
