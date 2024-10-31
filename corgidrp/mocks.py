added tests, fixed units labels, added a 'RN_UNIT' header key

corgidrp/mocks.py
@@ -111,7 +111,7 @@ def create_noise_maps(FPN_map, FPN_map_err, FPN_map_dq, CIC_map, CIC_map_err, CI
         exthdr['EMGAIN_M'] = None
     exthdr['CMDGAIN'] = None
     exthdr['KGAIN'] = None
-    exthdr['BUNIT'] = 'detected EM electrons'
+    exthdr['BUNIT'] = 'detected electrons'
     exthdr['HIERARCH DATA_LEVEL'] = None
     # simulate raw data filenames
     exthdr['DRPNFILE'] = 2
@@ -122,7 +122,7 @@ def create_noise_maps(FPN_map, FPN_map_err, FPN_map_dq, CIC_map, CIC_map_err, CI
     exthdr['B_O_ERR'] = 0.001
 
     err_hdr = fits.Header()
-    err_hdr['BUNIT'] = 'detected EM electrons'
+    err_hdr['BUNIT'] = 'detected electrons'
     exthdr['DATATYPE'] = 'DetectorNoiseMaps'
     input_data = np.stack([FPN_map, CIC_map, DC_map])
     err = np.stack([[FPN_map_err, CIC_map_err, DC_map_err]])

tests/e2e_tests/bp_map_e2e.py
@@ -70,7 +70,7 @@ def test_bp_map_master_dark_e2e(tvacdata_path, e2eoutput_path):
     noise_map_noise = np.zeros([1,] + list(noise_map_dat.shape))
     noise_map_dq = np.zeros(noise_map_dat.shape, dtype=int)
     err_hdr = fits.Header()
-    err_hdr['BUNIT'] = 'detected EM electrons'
+    err_hdr['BUNIT'] = 'detected electrons'
     ext_hdr['B_O'] = 0
     ext_hdr['B_O_ERR'] = 0
 

tests/e2e_tests/noisemap_cal_e2e.py
@@ -14,6 +14,7 @@ import corgidrp.data as data
 import corgidrp.mocks as mocks
 import corgidrp.walker as walker
 import corgidrp.caldb as caldb
+import corgidrp.detector as detector
 
 try:
     from cal.calibrate_darks.calibrate_darks_lsq import calibrate_darks_lsq 
@@ -173,9 +174,8 @@ def test_noisemap_calibration_from_l1(tvacdata_path, e2eoutput_path):
     assert(np.nanmax(np.abs(corgidrp_noisemap.data[1]- C_map)) < 1e-11)
     assert(np.nanmax(np.abs(corgidrp_noisemap.data[2]- D_map)) < 1e-11)
     assert(np.abs(corgidrp_noisemap.ext_hdr['B_O']- bias_offset) < 1e-11)
-
     pass
-    
+
     # for noise_ext in ["FPN_map","CIC_map","DC_map"]:
         # corgi_dat = detector.imaging_slice('SCI', corgidrp_noisemap.__dict__[noise_ext])
         # iit_dat = detector.imaging_slice('SCI', iit_noisemap.__dict__[noise_ext])