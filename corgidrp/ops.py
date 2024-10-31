7ce6cfe9c0c83d733155e710556785a8d3633759
Maxwell A. Millar-Blanchaer <maxmb@ucsb.edu>
2024-10-31 12:05:45 -0700

    added test and updated .gitignore

--- corgidrp/ops.py
+++ corgidrp/ops.py
@@ -12,5 +12,5 @@ def step_2_load_cal(this_caldb, main_cal_dir):
     this_caldb.scan_dir_for_new_entries(main_cal_dir)
     return this_caldb
 
-def step_3_process_data(input_filelist, cpgs_xml_filepath, outputdir, template =None):
+def step_3_process_data(input_filelist, cpgs_xml_filepath, outputdir, template=None):
      walker.walk_corgidrp(input_filelist, cpgs_xml_filepath, outputdir, template=template)
