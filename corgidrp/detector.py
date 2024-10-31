added tests, fixed units labels, added a 'RN_UNIT' header key

corgidrp/detector.py
@@ -306,7 +306,7 @@ def embed(data, arrtype, key, pad_val=0, detector_regions=None):
             detector_regions = detector_areas
         ff_rows = detector_regions[arrtype]['frame_rows']
         ff_cols = detector_regions[arrtype]['frame_cols']
-        full_frame = pad_val*np.ones(ff_rows, ff_cols)
+        full_frame = pad_val*np.ones((ff_rows, ff_cols))
         rows, cols, r0c0 = unpack_geom(arrtype, key, detector_regions)
         try:
             full_frame[r0c0[0]:r0c0[0]+rows, r0c0[1]:r0c0[1]+cols] = data

tests/e2e_tests/trap_pump_cal_e2e.py
@@ -145,7 +145,7 @@ def test_trap_pump_cal(tvacdata_path, e2eoutput_path, e2e=True):
     noise_map_noise = np.zeros([1,] + list(noise_map_dat.shape))
     noise_map_dq = np.zeros(noise_map_dat.shape, dtype=int)
     err_hdr = fits.Header()
-    err_hdr['BUNIT'] = 'detected EM electrons'
+    err_hdr['BUNIT'] = 'detected electrons'
     # from CGI_TVAC_Data/TV-20_EXCAM_noise_characterization/tvac_noisemap_original_data/results/bias_offset.txt
     ext_hdr['B_O'] = 0 # bias offset not simulated in the data, so set to 0;  -0.0394 DN from tvac_noisemap_original_data/results
     ext_hdr['B_O_ERR'] = 0 # was not estimated with the II&T code