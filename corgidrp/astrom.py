34c71f4183e768d129fcefd8f41136ba1b77600a
manduhmia <79128537+manduhmia@users.noreply.github.com>
2024-12-17 10:30:27 -0800

    fixing input error in measuring star offsets for computing plate scale and north angle

--- corgidrp/astrom.py
+++ corgidrp/astrom.py
@@ -507,7 +507,7 @@ def compute_platescale_and_northangle(image, source_info, center_coord, center_r
         xguess = star2['x'] - star1['x']
         yguess = star2['y'] - star1['y']
         
-        xoff, yoff = measure_offset(image, xstar_guess=star1['x'], ystar_guess=star2['x'], xoffset_guess= xguess, yoffset_guess= yguess)
+        xoff, yoff = measure_offset(image, xstar_guess=star1['x'], ystar_guess=star1['y'], xoffset_guess= xguess, yoffset_guess= yguess)
 
         pixsep = np.sqrt(np.power(xoff,2) + np.power(yoff,2))
         pixseps[i] = pixsep
