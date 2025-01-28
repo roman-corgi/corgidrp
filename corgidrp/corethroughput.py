2310f78108e8bf14e666909c4ef47f64f881fbfd
Sergi Hildebrandt <srh@caltech.edu>
2025-01-27 18:09:05 -0800

    Update corethroughput.py

--- corgidrp/corethroughput.py
+++ corgidrp/corethroughput.py
@@ -8,33 +8,6 @@ from corgidrp.data import Dataset
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-# CTC requirements
-"""
-1090881 - Given a core throughput dataset consisting of M clean frames 
-(nominally 1024x1024) taken at different FSM positions, the CTC GSW shall
-estimate the pixel location and core throughput of each PSF.
-
-NOTE: the list of M clean frames may be a subset of the frames collected during
-core throughput data collection, to allow for the removal of outliers.
-
-1090882 - Given 1) the location of the center of the FPM coronagraphic mask in
-EXCAM pixels during the coronagraphic observing sequence and 2) the FPAM and
-FSAM encoder positions during both the coronagraphic and core throughput observing
-sequences, the CTC GSW shall compute the center of the FPM coronagraphic mask
-during the core throughput observing sequence. 
-
-1090883 - Given 1) an array of PSF pixel locations and 2) the location of the
-center of the FPAM coronagraphic mask in EXCAM pixels during core throughput
-calibrations, and 3) corresponding core throughputs for each PSF, the CTC GSW
-shall compute a 2D floating-point interpolated core throughput map.
-
-1090884 - Given 1) a core throughput dataset consisting of a set of clean frames
-(nominally 1024x1024) taken at different FSM positions, and 2) a list of N (x, y)
-coordinates, in units of EXCAM pixels, which fall within the area covered by the
-core throughput dataset, the CTC GSW shall produce a 1024x1024xN cube of PSF
-images best centered at each set of coordinates.
-"""
-
 def get_psf_pix(
     dataset,
     method='max',
