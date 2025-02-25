e776840e70606b4b880c5a1d23145d57d7f63abb
Julia Milton <jmzarina@gmail.com>
2025-02-24 23:21:15 -0800

    changed naming for detected electrons

--- tests/test_calibrate_darks_lsq.py
+++ tests/test_calibrate_darks_lsq.py
@@ -121,15 +121,15 @@ def test_expected_results_sub():
     # check headers
     assert(noise_maps.data.ndim == 3)
     assert('DetectorNoiseMaps' in noise_maps.filename)
-    assert(noise_maps.ext_hdr["BUNIT"] == "Photoelectrons")
-    assert(noise_maps.err_hdr["BUNIT"] == "Photoelectrons")
+    assert(noise_maps.ext_hdr["BUNIT"] == "Detected Electrons")
+    assert(noise_maps.err_hdr["BUNIT"] == "Detected Electrons")
     assert("DetectorNoiseMaps" in str(noise_maps.ext_hdr["HISTORY"]))
     assert(noise_maps.ext_hdr['B_O_UNIT'] == 'DN')
 
     assert(nm_open.data.ndim == 3)
     assert('DetectorNoiseMaps' in nm_open.filename)
-    assert(nm_open.ext_hdr["BUNIT"] == "Photoelectrons")
-    assert(nm_open.err_hdr["BUNIT"] == "Photoelectrons")
+    assert(nm_open.ext_hdr["BUNIT"] == "Detected Electrons")
+    assert(nm_open.err_hdr["BUNIT"] == "Detected Electrons")
     assert("DetectorNoiseMaps" in str(nm_open.ext_hdr["HISTORY"]))
     assert(nm_open.ext_hdr['B_O_UNIT'] == 'DN')
 
