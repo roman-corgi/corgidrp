d1c741960edfcf845ae5aa8841aa9cc18fcd612d
manduhmia <79128537+manduhmia@users.noreply.github.com>
2025-03-04 10:56:07 -0800

    adding a platescale ext_hdr keyword into the create wcs function

--- corgidrp/l2b_to_l3.py
+++ corgidrp/l2b_to_l3.py
@@ -52,6 +52,8 @@ def create_wcs(input_dataset, astrom_calibration):
         wcs_info['CRVAL1'] = center_coord[0]
         wcs_info['CRVAL2'] = center_coord[1]
 
+        wcs_info['PLTSCALE'] = platescale  ## [mas] / pixel
+
         # update the image header with wcs information
         for key, value in wcs_info.items():
             image.ext_hdr[key] = value
