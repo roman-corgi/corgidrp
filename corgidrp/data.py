a0fe8f9f9a4e55c10c7eedbc6d4a5c069e06ebef
Alexis Lau <alexis.yslau@gmail.com>
2025-03-07 02:10:54 +0100

    Change the definition of hot_warm_pixels and dead_pixels back to uint8. Also enforce the endianness to make sure we are getting the right bits

--- corgidrp/data.py
+++ corgidrp/data.py
@@ -2173,8 +2173,9 @@ def unpackbits_64uint(arr, axis):
     Returns:
         _type_: np.ndarray of bits
     """
-    n = np.array(arr).view("u1")
-    return np.unpackbits(n, axis=axis, bitorder='little')
+    arr = arr.astype('>u8')
+    n = arr.view('u1')
+    return np.unpackbits(n, axis=axis, bitorder='big')
 
 def packbits_64uint(arr, axis):
     """
@@ -2187,4 +2188,4 @@ def packbits_64uint(arr, axis):
     Returns:
         _type_: np.ndarray of 64-bit unsigned integers
     """
-    return np.packbits(arr, axis=axis, bitorder='little').view(np.uint64)
+    return np.packbits(arr, axis=axis, bitorder='big').view('>u8')
