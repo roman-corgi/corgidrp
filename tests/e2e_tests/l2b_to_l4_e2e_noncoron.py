c90ec8f855c71bcfa3da982148ffd51fb5858c2f
Max Millar-Blanchaer <maxmb@ucsb.edu>
2025-04-01 11:08:26 -0700

    adding BIAS HDU to noncoron L3 e2e

--- tests/e2e_tests/l2b_to_l4_e2e_noncoron.py
+++ tests/e2e_tests/l2b_to_l4_e2e_noncoron.py
@@ -120,9 +120,16 @@ def test_l2b_to_l3(e2edata_path, e2eoutput_path):
         new_psf_center_x = psf_center_x + col_start
         new_psf_center_y = psf_center_y + row_start
 
+        #Make a BIAS HDU
+        bias_hdu = fits.ImageHDU(data=np.zeros_like(big_array[0]))
+        bias_hdu.nane = 'BIAS'
+        bias_hdu.header['PCOUNT'] = 0
+        bias_hdu.header['GCOUNT'] = 1
+        bias_hdu.header['EXTNAME'] = 'BIAS'
+
         #Create the new Image object
         mock_pri_header, mock_ext_header = create_default_L2b_headers()
-        new_image = Image(big_array, mock_pri_header, mock_ext_header, err=big_err)
+        new_image = Image(big_array, mock_pri_header, mock_ext_header, err=big_err, input_hdulist=[bias_hdu])
         # new_image.ext_hdr.set('PSF_CEN_X', new_psf_center_x)
         # new_image.ext_hdr.set('PSF_CEN_Y', new_psf_center_y)
         new_image.pri_hdr.set('FRAMET', 1)
