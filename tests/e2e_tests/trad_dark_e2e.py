a1558207e4523fdf2604a6b0651d64052b178d6a
Kevin Ludwick <kevin.ludwick.ext@ierustech.com>
2025-04-01 10:53:49 -0500

    Update trad_dark_e2e.py

--- tests/e2e_tests/trad_dark_e2e.py
+++ tests/e2e_tests/trad_dark_e2e.py
@@ -427,16 +427,16 @@ def test_trad_dark_im(tvacdata_path, e2eoutput_path):
     TVAC_trad_dark = detector.slice_section(mean_frame, 'SCI', 'image')
 
     assert(np.nanmax(np.abs(TVAC_trad_dark - trad_dark)) < 1e-11)
-    trad_dark = data.Dark(generated_trad_dark_file.replace("_L1_", "_L2a_", 1))
+    trad_dark = data.Dark(generated_trad_dark_file)
     assert trad_dark.ext_hdr['BUNIT'] == 'Detected Electrons'
     assert trad_dark.err_hdr['BUNIT'] == 'Detected Electrons'
-    test_filename = trad_dark_data_filelist[-1].split('.fits')[0] + '_DNM_CAL.fits'
+    test_filepath = trad_dark_data_filelist[-1].split('.fits')[0] + '_DRK_CAL.fits'
+    test_filename = os.path.basename(test_filepath)
     test_filename = re.sub('_L[0-9].', '', test_filename)
     assert(trad_dark.filename == test_filename)
     pass
 
     # remove from caldb
-    trad_dark = data.Dark(generated_trad_dark_file.replace("_L1_", "_L2a_", 1))
     this_caldb.remove_entry(trad_dark)
 
 if __name__ == "__main__":
