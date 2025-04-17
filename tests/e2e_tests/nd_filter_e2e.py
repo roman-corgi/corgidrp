5a135f00b184a2ded0203d196a016fc7f4aa32cf
juliamilton <jmzarina@gmail.com>
2025-04-17 10:38:47 -0700

    changed file output locations

--- tests/e2e_tests/nd_filter_e2e.py
+++ tests/e2e_tests/nd_filter_e2e.py
@@ -9,7 +9,7 @@ from corgidrp import caldb
 
 # ----------------------------------------------------------------------
 @pytest.mark.e2e
-def test_nd_filter_e2e(e2edata_path, e2eoutput_path):
+def test_nd_filter_e2e(e2edata_path):
     # 1. Synthetic “dim star” frames (no ND)
     fwhm = 3  # pix PSF width
     true_flux_dim = nd_filter_calibration.compute_expected_band_irradiance('TYC 4424-1286-1', '3C')
@@ -49,14 +49,12 @@ def test_nd_filter_e2e(e2edata_path, e2eoutput_path):
 
     # 4. Run the DRP walker with outputs saved in the current folder (e2eoutput_path)
     # Remove old NDF cal files first
-    for old_file in glob.glob(os.path.join(e2eoutput_path, "*NDF_CAL.fits")):
+    for old_file in glob.glob(os.path.join(simdata_dir, "*NDF_CAL.fits")):
         os.remove(old_file)
-    out_dir = e2eoutput_path
-    #walker.walk_corgidrp(filelist, "", out_dir, template='l2b_to_nd_filter.json')
-    walker.walk_corgidrp(filelist, "", out_dir)
+    walker.walk_corgidrp(filelist, "", simdata_dir)
 
     # 5. Load product & assert if calculated OD matches the input
-    nd_file = glob.glob(os.path.join(out_dir, "*_NDF_CAL*.fits"))
+    nd_file = glob.glob(os.path.join(simdata_dir, "*_NDF_CAL*.fits"))
     nd_cal  = data.NDFilterSweetSpotDataset(nd_file[0])
 
     recovered_od = float(nd_cal.od_values[0])  # use the first entry for the check
@@ -76,4 +74,4 @@ if __name__ == "__main__":
     parser.add_argument("-tvac", "--e2edata_dir", default=here)
     parser.add_argument("-o",    "--outputdir",   default=here)
     args = parser.parse_args()
-    test_nd_filter_e2e(args.e2edata_dir, args.outputdir)
+    test_nd_filter_e2e(args.e2edata_dir)
