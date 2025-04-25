check for correct units in relevant pipeline steps

corgidrp/mocks.py
@@ -1755,7 +1755,7 @@ def create_not_normalized_dataset(filedir=None, numfiles=10):
     frames = []
     for i in range(numfiles):
         # TO DO: Determine what level this image should be
-        prihdr, exthdr = create_default_L1_headers()
+        prihdr, exthdr = create_default_L2b_headers()
 
         sim_data = np.asarray(np.random.poisson(lam=150.0, size=(1024,1024)), dtype=float)
         sim_err = np.asarray(np.random.poisson(lam=1.0, size=(1024,1024)), dtype=float)
@@ -2853,7 +2853,7 @@ def create_flux_image(star_flux, fwhm, cal_factor, filter='3C', fpamname = 'HOLE
         fpam_v = 6124.9
 
     # Create image object
-    prihdr, exthdr = create_default_L2b_headers()
+    prihdr, exthdr = create_default_L4_headers()
     prihdr['VISTYPE'] = 'ABSFLXBT'
     prihdr['RA'] = target_location[0]
     prihdr['DEC'] = target_location[1]

tests/test_kgain.py
@@ -28,13 +28,15 @@ def test_kgain():
 
     gain_value = np.array([[9.55]])
     gain_err = np.array([[[1.]]])
-    kgain = data.KGain(gain_value, pri_hdr = prhd, ext_hdr = exthd, input_dataset = dataset)
+    prhd_kgain = prhd.copy()
+    exthd_kgain = exthd.copy()
+    kgain = data.KGain(gain_value, pri_hdr = prhd_kgain, ext_hdr = exthd_kgain, input_dataset = dataset)
     assert kgain.filename.split(".")[0] == "test2_KRN_CAL"
     assert kgain.value == gain_value[0,0]
     assert kgain.data[0,0] == gain_value[0,0]
     
     #test ptc and error extension
-    kgain_ptc = data.KGain(gain_value, err = gain_err, ptc = ptc, pri_hdr = prhd, ext_hdr = exthd, ptc_hdr = ptc_hdr, input_dataset = dataset)
+    kgain_ptc = data.KGain(gain_value, err = gain_err, ptc = ptc, pri_hdr = prhd_kgain, ext_hdr = exthd_kgain, ptc_hdr = ptc_hdr, input_dataset = dataset)
     assert kgain_ptc.error == gain_err[0,0]
     assert kgain_ptc.ptc[0,0] == 1.
     assert kgain_ptc.ptc_hdr is not None