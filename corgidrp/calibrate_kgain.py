a24a9107ff6b93a86bd5adc6b3ddfa69a6c9d57a
Eric Shen <ericshen567890@gmail.com>
2025-08-19 13:18:32 -0700

    added nonlin kgain error supression again

--- corgidrp/calibrate_kgain.py
+++ corgidrp/calibrate_kgain.py
@@ -121,8 +121,10 @@ def ptc_bin2(frame_in, mean_frame, binwidth, max_DN):
                 
             # Ensure that there is at least one element to calculate mean and std
             if np.any(mean_idx):
-                local_mean_array[m, n] = np.nanmean(frame_in_flat[mean_idx])
-                local_noise_array[m, n] = np.nanstd(frame_in_flat[mean_idx])
+                with warnings.catch_warnings():
+                    warnings.filterwarnings('ignore', category=RuntimeWarning)
+                    local_mean_array[m, n] = np.nanmean(frame_in_flat[mean_idx])
+                    local_noise_array[m, n] = np.nanstd(frame_in_flat[mean_idx])
             else:
                 local_mean_array[m, n] = 0
                 local_noise_array[m, n] = 0
@@ -525,7 +527,9 @@ def calibrate_kgain(dataset_kgain,
             in range(len(frames_diff))]
         
         # split each frame up into bins, take std and mean of each region
-        mean_frames_mean_curr0 = np.nanmean(frames, axis=0)
+        with warnings.catch_warnings():
+            warnings.filterwarnings('ignore', category=RuntimeWarning)
+            mean_frames_mean_curr0 = np.nanmean(frames, axis=0)
         mean_frames_mean_curr = np.nanmean(mean_frames_mean_curr0[rowroi,colroi])
         
         # Calculate the means
