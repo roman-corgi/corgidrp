e22f6a073c611f901a65ef9d7f7e68eba2ab4cd0
Eric Shen <ericshen567890@gmail.com>
2025-08-19 21:27:53 -0700

    removed duplicate import statement

--- corgidrp/combine.py
+++ corgidrp/combine.py
@@ -4,7 +4,6 @@ Module to support frame combination
 import warnings
 import numpy as np
 import corgidrp.data as data
-import warnings
 
 
 def combine_images(data_subset, err_subset, dq_subset, collapse, num_frames_scaling):
