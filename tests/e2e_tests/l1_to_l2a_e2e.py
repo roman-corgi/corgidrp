b9d91bfeb72564ed35186e3eb3ae4fa55f538b07
juliamilton <jmzarina@gmail.com>
2026-01-16 16:40:10 -0800

    Reset selected e2e tests to develop

--- tests/e2e_tests/l1_to_l2a_e2e.py
+++ tests/e2e_tests/l1_to_l2a_e2e.py
@@ -231,7 +231,7 @@ if __name__ == "__main__":
     # defaults allowing the use to edit the file if that is their preferred
     # workflow.
     #e2edata_dir = '/home/jwang/Desktop/CGI_TVAC_Data/'
-    e2edata_dir = '/Users/jmilton/Documents/CGI/E2E_Test_Data2'
+    e2edata_dir = '/Users/kevinludwick/Downloads/DRP E2E Test Files v2/E2E_Test_Data'
     outputdir = thisfile_dir
 
     ap = argparse.ArgumentParser(description="run the l1->l2a end-to-end test")
