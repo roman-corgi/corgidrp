fix how k gain /nonlin handles cosmic rays, fix effective fwc used for cosmic masking, updated to CBE params

corgidrp/data.py
@@ -1827,8 +1827,8 @@ class DetectorParams(Image):
     # default detector params
     default_values = {
         'KGAINPAR' : 8.7,
-        'FWC_PP_E' : 90000.,
-        'FWC_EM_E' : 100000.,
+        'FWC_PP_E' : 90000.,    # full-well capacity in electrons for image area (before gain register)
+        'FWC_EM_E' : 105000.,   # full-well capacity in electrons for gain register
         'ROWREADT' : 223.5e-6,  # seconds
         'NEMGAIN': 604,         # number of EM gain register stages
         'TELRSTRT': -1,         # slice of rows that are used for telemetry
@@ -1838,8 +1838,8 @@ class DetectorParams(Image):
         'GAINMAX': 8000.0,      # Maximum allowable EM gain
         'DELCNST': 1.0e-4,      # tolerance in exposure time calculator
         'OVERHEAD': 3,          # Overhead time, in seconds, for each collected frame.  Used to compute total wall-clock time for data collection
-        'PCECNTMX': 0.1,        # Maximum allowed electrons/pixel/frame for photon counting
-        'TFACTOR': 5,            # number of read noise standard deviations at which to set the photon-counting threshold
+        'PCECNTMX': 0.25,       # Maximum allowed electrons/pixel/frame for photon counting
+        'TFACTOR': 5,           # number of read noise standard deviations at which to set the photon-counting threshold
     }
 
     back_compat_mapping = {
@@ -4256,6 +4256,38 @@ def get_bit_to_flag_map():
         dict: A dictionary with bit positions (int) as keys and flag names as values.
     """
     return {bit: name for name, bit in get_flag_to_bit_map().items()}
+
+def selective_dq(dq, val=32):
+    '''Reduces the DQ map to just the flags valued at val.  This allows the ability to mask a frame because 
+    of a single DQ flag without regard to other flags. 
+    
+    For example, for val=32, the function identifies which pixels are flagged as saturated and creates a new map where those pixels are flagged as val and all other pixels are flagged as 0.
+    The function does this by iteratively identifying the highest power of 2 in the DQ map, 
+    checking if that power of 2 corresponds to the value for saturation, and if so, marking those pixels in the output map.  
+    Then it removes that power of 2 from the DQ map copy and repeats until there are no more flags left in the DQ map copy.
+    
+    To select for all DQs except for the one indicated by val, do dq - selective_dq(dq,val).
+
+    Args:
+        dq (array-like): DQ map where each pixel's value is a combination of powers of 2 corresponding to different flags.  For example, if a pixel has a value of 34, it means that it has flags corresponding to 32 and 2.
+        val (int): The specific flag value to identify in the DQ map.  Defaults to 32 for saturation.
+
+    Returns:
+        cosmic_pixels (array-like): A map where pixels that had the specified flag value in the original DQ map are marked as val, and all other pixels are marked as 0.
+    '''
+    cosmic_pixels = np.zeros_like(dq).astype(float)
+    temp_dq = dq.copy().astype(float)
+    nan_rows = np.array([]) # initialize
+    while nan_rows.size < temp_dq.size:
+        bad_rows, bad_cols = np.where(temp_dq == 0)
+        temp_dq[bad_rows, bad_cols] = np.nan
+        highest_power = np.floor(np.log(temp_dq)/np.log(2))
+        cosmic_rows, cosmic_cols = np.where(2**(highest_power) == val)
+        cosmic_pixels[cosmic_rows,cosmic_cols] = val
+        temp_dq = temp_dq - 2**(highest_power)
+        nan_rows, nan_cols = np.where(np.isnan(temp_dq))
+    return cosmic_pixels
+
 
 def get_stokes_intensity_image(stokes_image):
     """Return a copy containing only the Stokes I plane for photometry.

corgidrp/l1_to_l2a.py
@@ -157,8 +157,8 @@ def prescan_biassub(input_dataset, noise_maps=None, return_full_frame=False,
 
     return output_dataset
 
-def detect_cosmic_rays(input_dataset, detector_params, k_gain = None, sat_thresh=0.7,
-                       plat_thresh=0.7, cosm_filter=1, cosm_box=3, cosm_tail=10,
+def detect_cosmic_rays(input_dataset, detector_params, k_gain = None, sat_thresh=0.99,
+                       plat_thresh=0.85, cosm_filter=1, cosm_box=3, cosm_tail=10,
                        mode='image', detector_regions=None, pct_oversat_lim=20, dataset_copy=True):
     """
     Detects cosmic rays in a given dataset. Updates the DQ to reflect the pixels that are affected.
@@ -172,7 +172,7 @@ def detect_cosmic_rays(input_dataset, detector_params, k_gain = None, sat_thresh
         k_gain (corgidrp.data.KGain): KGain calibration file
         sat_thresh (float):
             Multiplication factor for the pixel full-well capacity (fwc) that determines saturated cosmic
-            pixels. Interval 0 to 1, defaults to 0.7. Lower numbers are more aggressive in flagging saturation.
+            pixels. Interval 0 to 1, defaults to 0.99. Lower numbers are more aggressive in flagging saturation.
         plat_thresh (float):
             Multiplication factor for pixel full-well capacity (fwc) that determines edges of cosmic
             plateau. Interval 0 to 1, defaults to 0.7. Lower numbers are more aggressive in flagging cosmic
@@ -261,21 +261,25 @@ def detect_cosmic_rays(input_dataset, detector_params, k_gain = None, sat_thresh
     # mask 1
     m1 = (crmasked_cube >= sat_fwcs_array) * sat_dqval
 
-    # run remove_cosmics() with fwc=fwc_em since tails only come from
-    # saturation in the gain register --> this is mask 2
+    # Mask 2:  captures cosmic rays.  If EM gain is 1, no cosmic tails made since 
+    # those are only made in gain register.  
     # Do a for loop since it's calling a for loop in the sub-routine anyway
     # and can't handle different 'FWC_EM's for different frames.
     m2 = np.zeros_like(crmasked_cube)
 
     for i in range(len(crmasked_cube)):
         arrtype = crmasked_dataset.frames[i].ext_hdr['ARRTYPE']
+        if emgain_list[i] == 1:
+            cosm_tail_i = 0
+        else:
+            cosm_tail_i = cosm_tail
         m2[i,:,:] = flag_cosmics(cube=crmasked_cube[i:i+1,:,:],
-                        fwc=fwcem_dn_arr[i],
-                        sat_thresh=sat_thresh,
+                        fwc=sat_fwcs[i],
+                        sat_thresh=sat_thresh, # same value across the frame, so just pick 0,0 
                         plat_thresh=plat_thresh,
                         cosm_filter=cosm_filter,
                         cosm_box=cosm_box,
-                        cosm_tail=cosm_tail,
+                        cosm_tail=cosm_tail_i,
                         mode=mode,
                         detector_regions=detector_regions,
                         arrtype=arrtype