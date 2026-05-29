d3d902de64a8b0169085c1c141629220b607b419
gstrampelli <strampelligiovanni@gmail.com>
2026-05-29 12:04:04 +0100

    corrected typo preventing CRPIX2 to be properly updated

--- corgidrp/l3_to_l4.py
+++ corgidrp/l3_to_l4.py
@@ -940,7 +940,7 @@ def northup(input_dataset,use_wcs=True,rot_center='im_center',new_center=None):
         #update CPIX/STARLOC
         crpix1_rot, crpix2_rot = corgidrp.spec.rotate_points((sci_hd['CRPIX1'], sci_hd['CRPIX2']), np.deg2rad(rotation_angle), (xcen, ycen))
         sci_hd['CRPIX1'] = crpix1_rot
-        sci_hd['CRPIX12'] = crpix2_rot
+        sci_hd['CRPIX2'] = crpix2_rot
         try:
             starlocx_rot, starlocy_rot = corgidrp.spec.rotate_points((sci_hd['STARLOCX'], sci_hd['STARLOCY']), np.deg2rad(rotation_angle), (xcen, ycen))
             sci_hd['STARLOCX'] = starlocx_rot
