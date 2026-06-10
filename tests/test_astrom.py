026bdc991a3ead8332c1852a982e761d474c4ebe
manduhmia <79128537+manduhmia@users.noreply.github.com>
2026-06-10 17:03:25 -0500

    updating test_astrom to have no vignetting and adding function to test vignetted mock data

--- tests/test_astrom.py
+++ tests/test_astrom.py
@@ -32,12 +32,7 @@ def test_astrom():
     field_path = os.path.join(os.path.dirname(__file__), "test_data", "JWST_CALFIELD2020.csv")
     
     # create a dataset with dithers
-    # dataset = mocks.create_astrom_data(field_path=field_path, filedir=datadir, rotation=20, dither_pointings=4)
-    dataset = mocks.create_astrom_data(field_path=field_path, rotation=20, dither_pointings=4)
-
-    # image_path = os.path.join(datadir, 'simcal_astrom.fits')
-    # open the image
-    # dataset = data.Dataset([image_path])
+    dataset = mocks.create_astrom_data(field_path=field_path, rotation=20, dither_pointings=4, vignette_radius=None)
 
     # check the dataset format
     assert len(dataset) == 5  # one pointing + 4 dithers
@@ -82,6 +77,67 @@ def test_astrom():
     pickled = pickle.dumps(astrom_cal_2)
     pickled_astrom = pickle.loads(pickled)
     assert np.all((astrom_cal.data == pickled_astrom.data)) # check it is the same as the original
+
+def test_astrom_vignette(vignette_radius=3_460):
+    """ 
+    Generate a simulated image and test the astrometric calibration computation.
+    
+    """
+    # create a simulated image with source guesses and true positions
+    # check that the simulated image folder exists and create if not
+    datadir = os.path.join(os.path.dirname(__file__), "test_data", "simastrom")
+    if not os.path.exists(datadir):
+        os.mkdir(datadir)
+
+    field_path = os.path.join(os.path.dirname(__file__), "test_data", "JWST_CALFIELD2020.csv")
+    
+    # create a dataset with dithers
+    dataset = mocks.create_astrom_data(field_path=field_path, rotation=20, dither_pointings=4, vignette_radius=vignette_radius)
+
+    # check the dataset format
+    assert len(dataset) == 5  # one pointing + 4 dithers
+    assert isinstance(dataset[0], data.Image)
+
+    # perform the astrometric calibration
+    astrom_cal = astrom.boresight_calibration(input_dataset=dataset, field_path=field_path, find_threshold=25)
+
+    # the data was generated to have the following image properties
+    expected_platescale = 21.8
+    atol_platescale = 0.5
+
+    # check orientation is correct within 0.05 [deg]
+    # and plate scale is correct within 0.5 [mas] (arbitrary)
+    expected_northangle = 20
+    atol_northangle = 0.05
+    test_result_platescale = (astrom_cal.northangle == pytest.approx(expected_northangle, abs=atol_northangle))
+    print(f'\nPlate scale estimate from boresight_calibration() is accurate: {expected_platescale} +/- {atol_platescale}: ', end='')
+    print_pass() if test_result_platescale else print_fail()
+    assert test_result_platescale
+
+    test_result_northangle = (astrom_cal.northangle == pytest.approx(expected_northangle, abs=atol_northangle))
+    assert test_result_northangle
+
+    # check that the center is correct within 3 [mas]
+    # the simulated image should have zero offset
+    target = dataset[0].pri_hdr['RA'], dataset[0].pri_hdr['DEC']
+    ra, dec = astrom_cal.boresight
+    assert ra == pytest.approx(target[0], abs=8.333e-7)     # reported as ra offset
+    assert dec == pytest.approx(target[1], abs=8.333e-7)
+
+    # check they can be pickled (for CTC operations)
+    pickled = pickle.dumps(astrom_cal)
+    pickled_astrom = pickle.loads(pickled)
+    assert np.all((astrom_cal.data == pickled_astrom.data))
+
+    # save and check it can be pickled after save
+    astrom_cal.save(filedir=datadir, filename="astrom_cal_output.fits")
+    astrom_cal_2 = data.AstrometricCalibration(os.path.join(datadir, "astrom_cal_output.fits"))
+
+    # check they can be pickled (for CTC operations)
+    pickled = pickle.dumps(astrom_cal_2)
+    pickled_astrom = pickle.loads(pickled)
+    assert np.all((astrom_cal.data == pickled_astrom.data)) # check it is the same as the original
+
 
 def test_distortion():
     """ 
