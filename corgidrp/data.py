28ff86cc2cf318fae2d84b3bbf54e3f8dc022d17
Juergen Schreiber <schreiber@mpia.de>
2026-08-06 15:03:48 +0200

    when saving cast only np.ndarray in additional HDUs, and consider SPEC_DQ

--- corgidrp/data.py
+++ corgidrp/data.py
@@ -590,10 +590,13 @@ class Image():
         dqhdu = fits.ImageHDU(data=self.dq, header = self.dq_hdr)
         hdulist.append(dqhdu)
 
-        # Cast data in additional HDUs to the configured dtype before appending
+        # Cast data in additional HDUs to the configured dtype before appending excluding binary tables etc.
         for hdu in self.hdu_list:
-            if hdu.data is not None:
-                hdu.data = hdu.data.astype(corgidrp.image_dtype, copy=False)
+            if hdu.data is not None and type(hdu.data) == np.ndarray:
+                if hdu.name == "SPEC_DQ":
+                    hdu.data = hdu.data.astype(corgidrp.dq_dtype, copy=False)
+                else:
+                    hdu.data = hdu.data.astype(corgidrp.image_dtype, copy=False)
             hdulist.append(hdu)
 
         with warnings.catch_warnings():
