89cc3adaae290763ced5f032338a0933ce4a9a43
Jason Wang <jason.wang@outlook.com>
2026-09-08 14:37:49 -0700

    make fsm dither positions bigger for nd filter

--- tests/e2e_tests/nd_filter_e2e.py
+++ tests/e2e_tests/nd_filter_e2e.py
@@ -33,7 +33,7 @@ def test_nd_filter_e2e(e2edata_path, e2eoutput_path):
     attenuated_flux = true_flux_bright / (10 ** od_truth)
 
     fsm_positions = [(0.0, 0.0),                     
-                     (1.0, -1.0)]                   
+                     (50.0, -50.0)]                   
 
     bright_frames = []
     for fsm_x, fsm_y in fsm_positions:
